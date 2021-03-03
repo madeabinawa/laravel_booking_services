64699132c2133aaa8759f6362535afe0ffde3b2e
I Made Dwija Abinawa <madeabinawa@gmail.com>
2021-03-03 10:48:25 +0800

    create tables migration

---
+++ database/migrations/2021_03_03_020630_create_assistants_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssistantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assistants', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+
+            $table->foreignId('manager_id')->nullable()->constrained();
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assistants');
+    }
+}
